f2ff4c903ce55304b158fbdb611b002732954bc6
Sean Fayfar <sfayfar@mit.edu>
2024-05-03 12:43:59 -0400

    Changed signals from PSD

--- diffractometer_controls/bluesky_config/startup/20-detectors.py
+++ diffractometer_controls/bluesky_config/startup/20-detectors.py
@@ -1,6 +1,6 @@
-from ophyd import Device, EpicsSignal, EpicsSignalRO, Component
+import numpy as np
 from ophyd import (Device, Component as Cpt,
-                   EpicsSignal, EpicsSignalRO, EpicsMotor)
+                   EpicsSignal, EpicsSignalRO, EpicsMotor, Signal)
 from ophyd.device import DeviceStatus
 from ophyd.status import Status, SubscriptionStatus
 
@@ -15,40 +15,42 @@ class EpicsSignalWithRBV(EpicsSignal):
         super().__init__(prefix + "_RBV", write_pv=prefix, **kwargs)
 
 class HE3PSD(Device):
-    acquire = Cpt(EpicsSignalWithRBV, ":Acquire")
-    acquire_time = Cpt(EpicsSignalWithRBV, ":AcquireTime")
-    nbins = Cpt(EpicsSignalWithRBV, ":NBins")
+    acquire = Cpt(EpicsSignalWithRBV, ":Acquire",kind='config')
+    acquire_time = Cpt(EpicsSignalWithRBV, ":AcquireTime",kind='config')
+    nbins = Cpt(EpicsSignalWithRBV, ":NBins",kind='config')
 
-    counts0 = Cpt(EpicsSignalRO, ":CountsD0")
-    counts1 = Cpt(EpicsSignalRO, ":CountsD1")
-    counts2 = Cpt(EpicsSignalRO, ":CountsD2")
-    counts3 = Cpt(EpicsSignalRO, ":CountsD3")
-    counts4 = Cpt(EpicsSignalRO, ":CountsD4")
-    counts5 = Cpt(EpicsSignalRO, ":CountsD5")
-    counts6 = Cpt(EpicsSignalRO, ":CountsD6")
-    counts7 = Cpt(EpicsSignalRO, ":CountsD7")
-    total_counts = Cpt(EpicsSignalRO, ":TotalCounts")
+    position_x = Cpt(Signal,value=np.linspace(-150,150,300))
 
-    live_counts0 = Cpt(EpicsSignalRO, ":LiveCountsD0")
-    live_counts1 = Cpt(EpicsSignalRO, ":LiveCountsD1")
-    live_counts2 = Cpt(EpicsSignalRO, ":LiveCountsD2")
-    live_counts3 = Cpt(EpicsSignalRO, ":LiveCountsD3")
-    live_counts4 = Cpt(EpicsSignalRO, ":LiveCountsD4")
-    live_counts5 = Cpt(EpicsSignalRO, ":LiveCountsD5")
-    live_counts6 = Cpt(EpicsSignalRO, ":LiveCountsD6")
-    live_counts7 = Cpt(EpicsSignalRO, ":LiveCountsD7")
-    live_total_counts = Cpt(EpicsSignalRO, ":LiveTotalCounts")
+    # counts0 = Cpt(EpicsSignalRO, ":CountsD0")
+    # counts1 = Cpt(EpicsSignalRO, ":CountsD1")
+    # counts2 = Cpt(EpicsSignalRO, ":CountsD2")
+    # counts3 = Cpt(EpicsSignalRO, ":CountsD3")
+    # counts4 = Cpt(EpicsSignalRO, ":CountsD4")
+    # counts5 = Cpt(EpicsSignalRO, ":CountsD5")
+    # counts6 = Cpt(EpicsSignalRO, ":CountsD6")
+    # counts7 = Cpt(EpicsSignalRO, ":CountsD7")
+    # total_counts = Cpt(EpicsSignalRO, ":TotalCounts")
 
-    _default_read_attrs = (
-        "counts0",
-        "counts7",
-        "total_counts"
-    )
+    counts0 = Cpt(EpicsSignalRO, ":LiveCountsD0")
+    counts1 = Cpt(EpicsSignalRO, ":LiveCountsD1",kind='omitted')
+    counts2 = Cpt(EpicsSignalRO, ":LiveCountsD2",kind='omitted')
+    counts3 = Cpt(EpicsSignalRO, ":LiveCountsD3",kind='omitted')
+    counts4 = Cpt(EpicsSignalRO, ":LiveCountsD4",kind='omitted')
+    counts5 = Cpt(EpicsSignalRO, ":LiveCountsD5",kind='omitted')
+    counts6 = Cpt(EpicsSignalRO, ":LiveCountsD6",kind='omitted')
+    counts7 = Cpt(EpicsSignalRO, ":LiveCountsD7")
+    total_counts = Cpt(EpicsSignalRO, ":LiveTotalCounts")
 
-    _default_configuration_attrs = (
-        "acquire_time",
-        "nbins"
-    )
+    # _default_read_attrs = (
+    #     "counts0",
+    #     "counts7",
+    #     "total_counts"
+    # )
+
+    # _default_configuration_attrs = (
+    #     "acquire_time",
+    #     "nbins"
+    # )
 
     def trigger(self):
         def check_value(*, old_value, value, **kwargs):
@@ -60,4 +62,4 @@ class HE3PSD(Device):
         return status
     
 
-det1 = HE3PSD("4dh4:det1", name="det1")
+he3psd = HE3PSD("4dh4:det1", name="he3psd")
