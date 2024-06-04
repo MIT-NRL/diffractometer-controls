33e75ff35eb6e3a42d45f3f85f212782a58c0f7a
Sean Fayfar <93339634+sfayfar@users.noreply.github.com>
2024-06-04 17:15:30 -0400

    Added EPICS simulated motor

---
+++ diffractometer_controls/bluesky_config/startup/01-sim_devices.py
@@ -0,0 +1,9 @@
+import numpy as np
+from ophyd import (Device, Component as Cpt,
+                   EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV, 
+                   EpicsMotor, Signal)
+from ophyd.device import DeviceStatus
+from ophyd.status import Status, SubscriptionStatus
+
+
+sim_motor = EpicsMotor("4dh4:m6",name="sim_motor")
