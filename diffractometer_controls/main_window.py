9fb4358a4b31e2fa2ee2832ca0289e56e7fc550a
Sean Fayfar <sfayfar@mit.edu>
2024-06-20 13:20:35 -0400

    Add button to launch all control screens

--- diffractometer_controls/main_window.py
+++ diffractometer_controls/main_window.py
@@ -7,9 +7,10 @@ import qtawesome as qta
 from pydm import data_plugins
 from pydm.main_window import PyDMMainWindow
 from qtpy import QtCore, QtGui
+from qtpy.QtCore import Qt, QTimer, Slot, QSize, QLibraryInfo
 from qtpy.QtWidgets import (QVBoxLayout, QHBoxLayout, QGroupBox,
     QLabel, QLineEdit, QPushButton, QScrollArea, QFrame,
-    QApplication, QWidget, QLabel)
+    QApplication, QWidget, QLabel, QAction)
 from bluesky_widgets.qt.run_engine_client import (
     QtReConsoleMonitor,
     QtReEnvironmentControls,
@@ -23,7 +24,7 @@ from bluesky_widgets.qt.run_engine_client import (
     QtReStatusMonitor,
 )
 from bluesky_widgets.models.run_engine_client import RunEngineClient
-from pydm.widgets import PyDMByteIndicator
+from pydm.widgets import PyDMByteIndicator, PyDMRelatedDisplayButton
 
 
 class MITRMainWindow(PyDMMainWindow):
@@ -45,6 +46,20 @@ class MITRMainWindow(PyDMMainWindow):
             heartbeat_indicator.labelPosition = 2
 
             bar.addPermanentWidget(heartbeat_indicator)
+
+            gear_icon = qta.icon('fa.gear')
+            # action = QAction(gear_icon, 'Controls', self)
+            controls = PyDMRelatedDisplayButton(filename="/home/mitr_4dh4/EPICS/IOCs/4dh4/4dh4App/op/adl/ioc_motors.adl")
+            controls.setText("Controls")
+            controls.setIcon(gear_icon)
+            controls.openInNewWindow = True
+            #move the label to below the icon
+            controls.iconPosition = 0
+            # set the size of the icon
+            controls.setIconSize(QSize(25, 25))
+
+            self.ui.navbar.addWidget(controls)
+
 
         def update_window_title(self):
             if self.showing_file_path_in_title_bar:
