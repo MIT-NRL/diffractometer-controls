09d09041c14c301f337d4a3c26ff92fdf5e358ba
Sean Fayfar <93339634+sfayfar@users.noreply.github.com>
2024-08-16 10:37:53 -0400

    Added pseudopositioner for analyzer curvature

--- diffractometer_controls/bluesky_config/startup/10-motors.py
+++ diffractometer_controls/bluesky_config/startup/10-motors.py
@@ -1,9 +1,10 @@
 import numpy as np
-from ophyd import (Device, Component as Cpt,
+from ophyd import (Device, Component as Cpt, FormattedComponent as FCpt,
                    EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV, 
                    EpicsMotor, Signal)
 from ophyd.device import DeviceStatus
 from ophyd.status import Status, SubscriptionStatus
+from ophyd.pseudopos import (PseudoPositioner, PseudoSingle, real_position_argument, pseudo_position_argument)
 
 
 # Tomography/Imaging motors
@@ -19,6 +20,31 @@ sample_th = EpicsMotor("4dh4:m10",name="sample_th",labels=["positioner"])
 det_psd_x = EpicsMotor("4dh4:m9",name="det_psd_x",labels=["positioner"])
 
 
-analyzer1_curve = EpicsMotor("4dh4:m11",name="analyzer1_curve")
-analyzer2_curve = EpicsMotor("4dh4:m15",name="analyzer2_curve")
+# analyzer1_curve = EpicsMotor("4dh4:m11",name="analyzer1_curve")
+# analyzer2_curve = EpicsMotor("4dh4:m15",name="analyzer2_curve")
 analyzer1_angle = EpicsMotor("4dh4:m13",name="analyzer1_angle")
+
+class AnalyzerCurvature(PseudoPositioner):
+    def __init__(self,
+                 analyzer_motor_pv: str,
+                 *args,
+                 **kwargs
+                 ):
+        self.analyzer_motor_pv = analyzer_motor_pv
+        super().__init__(*args, **kwargs)
+        
+    curve = Cpt(PseudoSingle, limits=(0,0.7), egu='1/m')
+
+    counts = FCpt(EpicsMotor, "{analyzer_motor_pv}", name='counts')
+
+    @pseudo_position_argument
+    def forward(self, pseudo_pos):
+        return self.RealPosition(counts=pseudo_pos.curve/0.0005516111545194904)
+    
+    @real_position_argument
+    def inverse(self, real_pos):
+        return self.PseudoPosition(curve=real_pos.counts*0.0005516111545194904)
+    
+
+analyzer1_curve = AnalyzerCurvature("4dh4:m11",name="analyzer1_curve")
+analyzer2_curve = AnalyzerCurvature("4dh4:m15",name="analyzer2_curve")
