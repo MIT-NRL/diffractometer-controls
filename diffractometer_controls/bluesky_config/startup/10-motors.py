9b13748d35f2375dd53c8d84fe3ae6d9d7781b17
Sean Fayfar <93339634+sfayfar@users.noreply.github.com>
2024-08-22 11:03:47 -0400

    Added motors and baseline readings

--- diffractometer_controls/bluesky_config/startup/10-motors.py
+++ diffractometer_controls/bluesky_config/startup/10-motors.py
@@ -1,4 +1,5 @@
 import numpy as np
+from bluesky import SupplementalData, RunEngine
 from ophyd import (Device, Component as Cpt, FormattedComponent as FCpt,
                    EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV, 
                    EpicsMotor, Signal)
@@ -6,6 +7,7 @@ from ophyd.device import DeviceStatus
 from ophyd.status import Status, SubscriptionStatus
 from ophyd.pseudopos import (PseudoPositioner, PseudoSingle, real_position_argument, pseudo_position_argument)
 
+sd: SupplementalData
 
 # Tomography/Imaging motors
 sample_tomo_th = EpicsMotor("4dh4:m3",name="sample_tomo_th",labels=["positioner"])
@@ -14,15 +16,16 @@ sample_tomo_x = EpicsMotor("4dh4:m14",name="sample_tomo_x",labels=["positioner"]
 cam_focus = EpicsMotor("4dh4:m12",name="cam_focus",labels=["positioner"])
 cam_x = EpicsMotor("4dh4:m1",name="cam_x",labels=["positioner"])
 
+
+#===============================================================================#
 # Diffraction motors
 sample_th = EpicsMotor("4dh4:m10",name="sample_th",labels=["positioner"])
 
 det_psd_x = EpicsMotor("4dh4:m9",name="det_psd_x",labels=["positioner"])
 
 
-# analyzer1_curve = EpicsMotor("4dh4:m11",name="analyzer1_curve")
-# analyzer2_curve = EpicsMotor("4dh4:m15",name="analyzer2_curve")
 analyzer1_angle = EpicsMotor("4dh4:m13",name="analyzer1_angle")
+analyzer1_x = EpicsMotor("4dh4:m16",name="analyzer1_x")
 
 class AnalyzerCurvature(PseudoPositioner):
     def __init__(self,
@@ -48,3 +51,11 @@ class AnalyzerCurvature(PseudoPositioner):
 
 analyzer1_curve = AnalyzerCurvature("4dh4:m11",name="analyzer1_curve")
 analyzer2_curve = AnalyzerCurvature("4dh4:m15",name="analyzer2_curve")
+
+
+sd.baseline.append(sample_th)
+sd.baseline.append(det_psd_x)
+sd.baseline.append(analyzer1_angle)
+sd.baseline.append(analyzer1_x)
+sd.baseline.append(analyzer1_curve)
+sd.baseline.append(analyzer2_curve)
