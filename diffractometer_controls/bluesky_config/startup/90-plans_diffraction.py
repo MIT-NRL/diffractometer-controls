330af9a16b9398f83677b171922cf8d85b65b482
Sean Fayfar <93339634+sfayfar@users.noreply.github.com>
2024-10-01 11:37:35 -0400

    Added scans for he-3 detectors

--- diffractometer_controls/bluesky_config/startup/90-plans_diffraction.py
+++ diffractometer_controls/bluesky_config/startup/90-plans_diffraction.py
@@ -1,4 +1,5 @@
 # import bluesky.plans
+import bluesky.plan_patterns
 import bluesky.plans as bp
 from bluesky.plans import scan, count, grid_scan, rel_scan, rel_grid_scan
 
@@ -19,9 +20,53 @@ from ophyd.status import Status, SubscriptionStatus
 from epics import caput, caget, cainfo
 from functools import partial
 
+import functools
+import operator
+from cycler import cycler
+
+try:
+    # cytools is a drop-in replacement for toolz, implemented in Cython
+    from cytools import partition
+except ImportError:
+    from toolz import partition
+
+
 frame_type_sig = EpicsSignal("4dh4:TS:FrameType", name="frame_type_sig")
 
 # monitor_and_count = bpp.monitor_during_decorator([he3psd0.counts])(bp.count)
+
+
+def inner_product_individual_nums(args):
+    """Scan over one multi-motor trajectory.
+
+    Parameters
+    ----------
+    num : integer
+        number of steps
+    args : list of {Positioner, Positioner, int}
+        patterned like (``motor1, start1, stop1, num1, ..., motorN, startN, stopN, numN``)
+        Motors can be any 'setable' object (motor, temp controller, etc.)
+
+    Returns
+    -------
+    cyc : cycler
+    """
+    if len(args) % 4 != 0:
+        raise ValueError("Wrong number of positional arguments for 'inner_product'")
+
+    cyclers = []
+    for (
+        motor,
+        start,
+        stop,
+        num
+    ) in partition(4, args):
+        steps = np.linspace(start, stop, num=num, endpoint=True)
+        c = cycler(motor, steps)
+        cyclers.append(c)
+    return functools.reduce(operator.add, cyclers)
+
+
 
 
 def count_he3(
@@ -110,7 +155,7 @@ def scan_he3(
             start_pos:float, 
             stop_pos:float, 
             step:float,
-            return_to_start:bool = True,
+            return_to_original_position:bool = True,
             md:dict = None
               ):
     '''
@@ -120,10 +165,17 @@ def scan_he3(
     
     num_steps = int(round((stop_pos-start_pos)/step) + 1)
     step = (stop_pos-start_pos)/(num_steps-1)
+    total_time = num_steps*detector.acquire_time.get() # in seconds
 
     print("#===============#")
-    print(f"Starting tomography scan from {start_pos} to {stop_pos} \nin {num_steps} steps of {step} {motor.egu}.")
+    print(f"Starting scan of {motor.name} from {start_pos} to {stop_pos} \nin {num_steps} steps of {step} {motor.egu}.")
+    hours = total_time // 3600
+    minutes = (total_time % 3600) // 60
+    seconds = total_time % 60
+    print(f"The scan time is estimated to be {hours} hours, {minutes} minutes, and {seconds} seconds.")
     print("#===============#")
+
+    original_pos = motor.position
 
     if not isinstance(detector,list):
         detector = [detector]
@@ -160,7 +212,7 @@ def scan_he3(
     _md["hints"] = default_hints
     _md["hints"].update(md.get("hints", {}) or {})
     
-    @bpp.monitor_during_decorator([detector[0].counts])
+    # @bpp.monitor_during_decorator([detector[0]])
     @bpp.run_decorator(md=_md)
     def main_plan():
 
@@ -176,12 +228,209 @@ def scan_he3(
             for step in list(cycler):
                 yield from bps.one_nd_step(detector, step, pos_cache)
 
-        # print("Replace the sample, open the shutter, and press Resume to start the scan")
-        # yield from bps.checkpoint()
-        # yield from bps.pause()
         yield from inner_scan_nd()
 
-        if return_to_start:
-            yield from bps.mv(motor, start_pos)
+        if return_to_original_position:
+            yield from bps.mv(motor, original_pos)
 
     return(yield from main_plan())
+
+
+def scan_list_he3( 
+            title:str,
+            sample:str,
+            detector, 
+            motor, 
+            position_list:list,
+            return_to_original_position:bool = True,
+            md:dict = None
+              ):
+    '''
+    General scan for the imaging detector system.
+    '''
+
+    num_steps = len(position_list)
+    total_time = num_steps*detector.acquire_time.get() # in seconds
+
+    print("#===============#")
+    print(f"Starting scan of {motor.name} through the following positions:")
+    print(f"{position_list}")
+    hours = total_time // 3600
+    minutes = (total_time % 3600) // 60
+    seconds = total_time % 60
+    print(f"The scan time is estimated to be {hours} hours, {minutes} minutes, and {seconds} seconds.")
+    print("#===============#")
+
+    original_pos = motor.position
+
+    if not isinstance(detector,list):
+        detector = [detector]
+    # md_args = list(chain(*((repr(motor), start, stop) for motor, start_angle, stop_angle)))
+    motor_names = motor.name
+    md = md or {}
+    _md = {
+        "title": title,
+        "sample": sample,
+        "plan_args": {
+            "detectors": list(map(repr, detector)),
+            # "num": num,
+            # "args": md_args,
+        },
+        "plan_name": "scan_list_he3",
+        "plan_pattern": "inner_list_product",
+        "plan_pattern_module": plan_patterns.__name__,
+        "plan_pattern_args": dict(motor=motor.name, position_list=position_list, num_steps=num_steps),  # noqa: C408
+        "motors": motor_names,
+    }
+    _md.update(md)
+
+    x_fields = []
+    x_fields.extend(utils.get_hinted_fields(motor))
+
+    default_dimensions = [(x_fields, "primary")]
+
+    default_hints = {}
+    if len(x_fields) > 0:
+        default_hints.update(dimensions=default_dimensions)
+
+    # now add default_hints and override any hints from the original md (if
+    # exists)
+    _md["hints"] = default_hints
+    _md["hints"].update(md.get("hints", {}) or {})
+    
+    # @bpp.monitor_during_decorator([detector[0].counts])
+    @bpp.run_decorator(md=_md)
+    def main_plan():
+
+        for det in detector:
+            yield from bps.stage(det)
+        yield from bps.stage(motor)
+        
+        pos_cache = defaultdict(lambda: None)
+        cycler = plan_patterns.inner_list_product(args=[motor, position_list])
+
+        def inner_scan_nd():
+            # yield from bps.declare_stream(motor, *detector, name="primary")
+            for step in list(cycler):
+                yield from bps.one_nd_step(detector, step, pos_cache)
+
+        yield from inner_scan_nd()
+
+        if return_to_original_position:
+            yield from bps.mv(motor, original_pos)
+
+    return(yield from main_plan())
+
+
+def scan2D_he3( 
+            title:str,
+            sample:str,
+            detector, 
+            motor_outer, 
+            start_pos_outer:float, 
+            stop_pos_outer:float, 
+            step_outer:float,
+            motor_inner,
+            start_pos_inner:float,
+            stop_pos_inner:float,
+            step_inner:float,
+            return_to_original_positions:bool = True,
+            md:dict = None
+              ):
+    '''
+    General scan for the imaging detector system.
+    '''
+    motors = [motor_outer, motor_inner]
+    
+    num_steps_outer = int(round((stop_pos_outer-start_pos_outer)/step_outer) + 1)
+    step_outer = (stop_pos_outer-start_pos_outer)/(num_steps_outer-1)
+
+    num_steps_inner = int(round((stop_pos_inner-start_pos_inner)/step_inner) + 1)
+    step_inner = (stop_pos_inner-start_pos_inner)/(num_steps_inner-1)
+
+    total_steps = num_steps_outer*num_steps_inner
+    total_time = total_steps*detector.acquire_time.get()
+
+    print("#===============#")
+    print(f"Starting 2D outer scan of with \n{motor_outer.name} from {start_pos_outer} to {stop_pos_outer} \nin {num_steps_outer} steps of {step_outer} {motor_outer.egu}.")
+    print(f"with the inner scan of \n{motor_inner.name} from {start_pos_inner} to {stop_pos_inner} \nin {num_steps_inner} steps of {step_inner} {motor_inner.egu}.")
+    print(f"Total of {total_steps} steps.")
+    hours = total_time // 3600
+    minutes = (total_time % 3600) // 60
+    seconds = total_time % 60
+    print(f"The scan time is estimated to be {hours} hours, {minutes} minutes, and {seconds} seconds.")
+    print("#===============#")
+
+    original_pos_inner = motor_inner.position
+    original_pos_outer = motor_outer.position
+
+    if not isinstance(detector,list):
+        detector = [detector]
+    # md_args = list(chain(*((repr(motor), start, stop) for motor, start_angle, stop_angle)))
+    motor_names = tuple(motor.name for motor in motors)
+    md = md or {}
+    _md = {
+        "title": title,
+        "sample": sample,
+        "plan_args": {
+            "detectors": list(map(repr, detector)),
+            # "num": num,
+            # "args": md_args,
+        },
+        "plan_name": "scan2D_he3",
+        "plan_pattern": "inner_product",
+        "plan_pattern_module": plan_patterns.__name__,
+        "plan_pattern_args": dict(motor_outer=motor_outer.name, start_pos_outer=start_pos_outer, stop_pos_outer=stop_pos_outer, step_outer=step_outer, num_steps_outer=num_steps_outer, 
+                                  motor_inner=motor_inner.name, start_pos_inner=start_pos_inner, stop_pos_inner=stop_pos_inner, step_inner=step_inner, num_steps_inner=num_steps_inner),  
+        "motors": motor_names,
+    }
+    _md.update(md)
+
+    x_fields = []
+    for motor in motors:
+        x_fields.extend(utils.get_hinted_fields(motor))
+
+    default_dimensions = [(x_fields, "primary")]
+
+    default_hints = {}
+    if len(x_fields) > 0:
+        default_hints.update(dimensions=default_dimensions)
+
+    # now add default_hints and override any hints from the original md (if
+    # exists)
+    _md["hints"] = default_hints
+    _md["hints"].update(md.get("hints", {}) or {})
+    
+    # @bpp.monitor_during_decorator([detector[0].counts])
+    @bpp.run_decorator(md=_md)
+    def main_plan():
+
+        for det in detector:
+            yield from bps.stage(det)
+        yield from bps.stage(motor)
+        
+        pos_cache = defaultdict(lambda: None)
+        cycler = plan_patterns.outer_product(args=[motor_outer, start_pos_outer, stop_pos_outer, num_steps_outer, motor_inner, start_pos_inner, stop_pos_inner, num_steps_inner])
+
+        def inner_scan_nd():
+            # yield from bps.declare_stream(motor, *detector, name="primary")
+            for step in list(cycler):
+                yield from bps.one_nd_step(detector, step, pos_cache)
+
+        yield from inner_scan_nd()
+
+        if return_to_original_positions:
+            yield from bps.mv(motor_inner, original_pos_inner)
+            yield from bps.mv(motor_outer, original_pos_outer)
+
+    return(yield from main_plan())
+
+def move_motor(
+        motor,
+        position:float,
+):
+    '''
+    Move a motor to a specified position.
+    '''
+    yield from bps.stage(motor)
+    yield from bps.mv(motor,position)
