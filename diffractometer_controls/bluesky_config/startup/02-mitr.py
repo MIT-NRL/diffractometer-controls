84eb8bfe67e2cacd58cdf2bbc1f18a08d6683e6d
Sean Fayfar <93339634+sfayfar@users.noreply.github.com>
2025-06-12 12:33:23 -0400

    Added dwk channels

--- diffractometer_controls/bluesky_config/startup/02-mitr.py
+++ diffractometer_controls/bluesky_config/startup/02-mitr.py
@@ -13,6 +13,10 @@ RE: RunEngine
 reactor_power_6 = EpicsSignalRO("mitr:Power6",name="reactor_power_6")
 reactor_power_4 = EpicsSignalRO("mitr:Power4",name="reactor_power_4")
 reactor_power_thm = EpicsSignalRO("mitr:PowerThm",name="reactor_power_thm")
+reactor_power_dwk1 = EpicsSignalRO("mitr:DWK1",name="reactor_power_dwk1")
+reactor_power_dwk2 = EpicsSignalRO("mitr:DWK2",name="reactor_power_dwk2")
+reactor_power_dwk3 = EpicsSignalRO("mitr:DWK3",name="reactor_power_dwk3")
+reactor_power_dwk4 = EpicsSignalRO("mitr:DWK4",name="reactor_power_dwk4")
 
 sd.monitors.append(reactor_power_6)
 sd.baseline.append(reactor_power_6)
